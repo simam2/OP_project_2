c7078997ee0daae16b8837d0e88e1a024a7cbaec
simam2 <145399241+simam2@users.noreply.github.com>
2023-12-22 15:18:00 +0200

    Change Student Struct to a Class

--- student.h
+++ student.h
@@ -5,11 +5,38 @@
 
 using namespace std;
 
-struct Student {
+class Student
+{
+private:
     string name;
     string surname;
     vector<int> grades;
     int examGrade;
     float finalAvg;
     float finalMdn;
+
+public:
+    Student() {};
+    ~Student() {};
+
+    Student(string, bool);
+    Student(string, string);
+
+    void setNames(string, string);
+    void setExamGrade(int);
+    void addGrade(int);
+
+    void generateRandomGrades(int);
+    void calculateAvg();
+    void calculateMdn();
+
+    inline string getName() const { return name; }
+    inline string getSurname() const { return surname; }
+    inline vector<int> getGrades() const { return grades; }
+    inline int getExamGrade() const { return examGrade; }
+    inline float getAvg() const { return finalAvg; }
+    inline float getMdn() const { return finalMdn; }
 };
+
+bool compareByName(const Student&, const Student&);
+bool compareByAvg(const Student&, const Student&);
